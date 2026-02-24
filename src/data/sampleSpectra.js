8afbb30a56264f04c2d9e6404f468a937e57948b
James Kawagoe <jkawagoe@James-MBP.local>
2026-02-23 17:34:17 -0800

    added COS acetone, benzil, ethyl acetate, and methyl salicylate

--- src/data/sampleSpectra.js
+++ src/data/sampleSpectra.js
@@ -7,9 +7,15 @@ import spectrum_0 from '../../sample-spectra/100-41-4-IR.jdx?raw'
 import spectrum_1 from '../../sample-spectra/109-94-4-IR.jdx?raw'
 import spectrum_2 from '../../sample-spectra/111-36-4-IR.jdx?raw'
 import spectrum_3 from '../../sample-spectra/119-36-8-IR.jdx?raw'
-import spectrum_4 from '../../sample-spectra/141-78-6-IR.jdx?raw'
-import spectrum_5 from '../../sample-spectra/592-42-7-IR.jdx?raw'
-import spectrum_6 from '../../sample-spectra/6630-86-0-IR.jdx?raw'
+import spectrum_4 from '../../sample-spectra/134-62-3-IR.jdx?raw'
+import spectrum_5 from '../../sample-spectra/141-78-6-IR.jdx?raw'
+import spectrum_6 from '../../sample-spectra/592-42-7-IR.jdx?raw'
+import spectrum_7 from '../../sample-spectra/6630-86-0-IR.jdx?raw'
+import spectrum_8 from '../../sample-spectra/94-09-7-IR.jdx?raw'
+import spectrum_9 from '../../sample-spectra/Acetone 67-64-1 run1.dx?raw'
+import spectrum_10 from '../../sample-spectra/Benzil 134-81-6 run1.dx?raw'
+import spectrum_11 from '../../sample-spectra/Ethyl Acetate 141-78-6 run1.dx?raw'
+import spectrum_12 from '../../sample-spectra/Methyl Salicylate 119-36-8 run1.dx?raw'
 
 export const SAMPLE_SPECTRA = [
   {
@@ -57,6 +63,17 @@ export const SAMPLE_SPECTRA = [
     jdxContent: spectrum_3,
   },
   {
+    id: '134-62-3',
+    name: 'Deet',
+    casNumber: '134-62-3',
+    names: null,
+    functionalGroups: [],
+    owner: 'NIST Standard Reference Data Program\nCollection (C) 2018 copyright by the U.S. Secretary of Commerce\non behalf of the United States of America. All rights reserved.',
+    origin: 'NIST Mass Spectrometry Data Center',
+    citation: null,
+    jdxContent: spectrum_4,
+  },
+  {
     id: '141-78-6',
     name: 'ACETIC ACID, ETHYL ESTER',
     casNumber: '141-78-6',
@@ -65,7 +82,7 @@ export const SAMPLE_SPECTRA = [
     owner: 'COBLENTZ SOCIETY\nCollection (C) 2018 copyright by the U.S. Secretary of Commerce\non behalf of the United States of America. All rights reserved.',
     origin: 'DOW CORNING CORP.',
     citation: 'COBLENTZ NO. 4802; DOW CORNING CORP.',
-    jdxContent: spectrum_4,
+    jdxContent: spectrum_5,
   },
   {
     id: '592-42-7',
@@ -76,7 +93,7 @@ export const SAMPLE_SPECTRA = [
     owner: 'COBLENTZ SOC.\nCollection (C) 2018 copyright by the U.S. Secretary of Commerce\non behalf of the United States of America. All rights reserved.',
     origin: 'DOW CHEMICAL COMPANY',
     citation: null,
-    jdxContent: spectrum_5,
+    jdxContent: spectrum_6,
   },
   {
     id: '6630-86-0',
@@ -87,6 +104,61 @@ export const SAMPLE_SPECTRA = [
     owner: 'COBLENTZ SOCIETY\nCollection (C) 2018 copyright by the U.S. Secretary of Commerce\non behalf of the United States of America. All rights reserved.',
     origin: 'ANDERSON PHYSICAL LABORATORY',
     citation: null,
-    jdxContent: spectrum_6,
+    jdxContent: spectrum_7,
+  },
+  {
+    id: '94-09-7',
+    name: 'Benzocaine',
+    casNumber: '94-09-7',
+    names: null,
+    functionalGroups: [],
+    owner: 'NIST Standard Reference Data Program\nCollection (C) 2018 copyright by the U.S. Secretary of Commerce\non behalf of the United States of America. All rights reserved.',
+    origin: 'NIST Mass Spectrometry Data Center',
+    citation: null,
+    jdxContent: spectrum_8,
+  },
+  {
+    id: '67-64-1',
+    name: 'Acetone 67-64-1 run1',
+    casNumber: '67-64-1',
+    names: null,
+    functionalGroups: [],
+    owner: 'College of the Sequoias',
+    origin: 'College of the Sequoias',
+    citation: null,
+    jdxContent: spectrum_9,
+  },
+  {
+    id: '134-81-6',
+    name: 'Benzil 134-81-6 run1',
+    casNumber: '134-81-6',
+    names: null,
+    functionalGroups: [],
+    owner: null,
+    origin: null,
+    citation: null,
+    jdxContent: spectrum_10,
+  },
+  {
+    id: '141-78-6-Ethyl-Acetate-141-78-6-run1-dx',
+    name: 'Ethyl Acetate 141-78-61',
+    casNumber: '141-78-6',
+    names: null,
+    functionalGroups: [],
+    owner: null,
+    origin: null,
+    citation: null,
+    jdxContent: spectrum_11,
+  },
+  {
+    id: '119-36-8-Methyl-Salicylate-119-36-8-run1-dx',
+    name: 'Methyl Salicylate 119-36-81',
+    casNumber: '119-36-8',
+    names: null,
+    functionalGroups: [],
+    owner: null,
+    origin: null,
+    citation: null,
+    jdxContent: spectrum_12,
   }
 ]
